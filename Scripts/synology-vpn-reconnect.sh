125b723282e593d32d72ab256f4dd7fb95c6a2c4
Björn Lindström <bkhl@elektrubadur.se>
2016-10-08 11:11:37 +0700

    Kill synology VPN client before restart
    Before doing reconnect, kill any currently running instance of the synology VPN client.

--- Scripts/synology-vpn-reconnect.sh
+++ Scripts/synology-vpn-reconnect.sh
@@ -3,6 +3,7 @@
 if ping -c 3 8.8.8.8 -q; then
 	echo Connection is up
 else
+	/usr/syno/bin/synovpnc kill_client
 	cat <<EOF > /usr/syno/etc/synovpnclient/vpnc_connecting
 conf_name=NAME
 conf_id=ID
