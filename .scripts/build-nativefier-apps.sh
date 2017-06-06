4418f4a568bcc22939d8095bca97038a77bb3991
Björn Lindström <bkhl@elektrubadur.se>
2017-06-06 14:14:26 +0700

    Cleanup of nativefier app build commands

--- .scripts/build-nativefier-apps.sh
+++ .scripts/build-nativefier-apps.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 
 # TODO: Skip apps that already exist.
-# TODO: Check icon extension.
 # TODO: Create .desktop files.
 
 set -x
 set -e
 
-nativefier --name Deezer 'http://www.deezer.com' --single-instance  ~/Software/Deezer/
-nativefier --name Gmail 'https://mail.google.com' --single-instance --internal-urls '^https?://mail.google.com(|/.*)$' ~/Software/Gmail
-nativefier --name Hangouts 'https://hangouts.google.com/' --single-instance --internal-urls '^https?://hangouts.google.com(|/.*)$' ~/Software/Hangouts
-nativefier --name Messenger --icon ~/Software/Messenger/messenger.svg 'https://www.messenger.com' --single-instance ~/Software/Messenger/
-nativefier --name Outlook 'https://mail.dayjob.example.com/owa/' --single-instance --internal-urls '^https?://mail.dayjob.example.com(|/.*)$'  ~/Software/Outlook
-nativefier --name Skype 'https://web.skype.com/en/' --single-instance --internal-urls '^https?://web.skype.com(|/.*)$' ~/Software/Skype
-nativefier --name Todoist --icon ~/Software/Todoist/todoist.png --single-instance 'https://todoist.com' ~/Software/Todoist/
-nativefier --name WhatsApp --icon ~/Software/WhatsApp/whatsapp.png 'https://web.whatsapp.com' --single-instance --internal-urls '^https?://web.whatsapp.com(|/.*)$' ~/Software/WhatsApp/
+NATIVEFIER='nativefier --single-instance'
+
+${NATIVEFIER} --name Deezer 'http://www.deezer.com' ~/Software/Deezer/
+${NATIVEFIER} --name Gmail --internal-urls '^https?://mail.google.com(/.*)?$' 'https://mail.google.com' ~/Software/Gmail
+${NATIVEFIER} --name Hangouts --internal-urls '^https?://hangouts.google.com(/.*)?$' 'https://hangouts.google.com/' ~/Software/Hangouts
+${NATIVEFIER} --name Messenger 'https://www.messenger.com' ~/Software/Messenger/
+${NATIVEFIER} --name OGS 'https://online-go.com' ~/Software/OGS/
+${NATIVEFIER} --name Outlook --internal-urls '^https?://mail.dayjob.example.com(/.*)?$' 'https://mail.dayjob.example.com/owa/' ~/Software/Outlook
+${NATIVEFIER} --name Skype --internal-urls '^https?://web.skype.com(/.*)?$' 'https://web.skype.com/en/' ~/Software/Skype
+${NATIVEFIER} --name Slack --internal-urls '^https?://(.*\.)?slack.com(/.*)?$' 'https://dayjobapac.slack.com' ~/Software/Slack
+${NATIVEFIER} --name Todoist 'https://todoist.com' ~/Software/Todoist/
+${NATIVEFIER} --name WhatsApp --internal-urls '^https?://web.whatsapp.com(/.*)?$' 'https://web.whatsapp.com' ~/Software/WhatsApp/
