855d8684e0b61e2c6f76fab5d79e57772c14cf49
Björn Lindström <bkhl@elektrubadur.se>
2017-06-09 12:14:31 +0700

    Changed Slack nativefier app to be multi-instance.

--- .scripts/build-nativefier-apps.sh
+++ .scripts/build-nativefier-apps.sh
@@ -8,13 +8,13 @@ set -e
 
 NATIVEFIER='nativefier --single-instance'
 
-${NATIVEFIER} --name Deezer 'http://www.deezer.com' ~/Software/Deezer/
-${NATIVEFIER} --name Gmail --internal-urls '^https?://mail.google.com(/.*)?$' 'https://mail.google.com' ~/Software/Gmail
-${NATIVEFIER} --name Hangouts --internal-urls '^https?://hangouts.google.com(/.*)?$' 'https://hangouts.google.com/' ~/Software/Hangouts
-${NATIVEFIER} --name Messenger 'https://www.messenger.com' ~/Software/Messenger/
-${NATIVEFIER} --name OGS 'https://online-go.com' ~/Software/OGS/
-${NATIVEFIER} --name Outlook --internal-urls '^https?://mail.dayjob.example.com(/.*)?$' 'https://mail.dayjob.example.com/owa/' ~/Software/Outlook
-${NATIVEFIER} --name Skype --internal-urls '^https?://web.skype.com(/.*)?$' 'https://web.skype.com/en/' ~/Software/Skype
-${NATIVEFIER} --name Slack --internal-urls '^https?://(.*\.)?slack.com(/.*)?$' 'https://dayjobapac.slack.com' ~/Software/Slack
-${NATIVEFIER} --name Todoist 'https://todoist.com' ~/Software/Todoist/
-${NATIVEFIER} --name WhatsApp --internal-urls '^https?://web.whatsapp.com(/.*)?$' 'https://web.whatsapp.com' ~/Software/WhatsApp/
+nativefier --single-instance --name Deezer 'http://www.deezer.com' ~/Software/Deezer/
+nativefier --single-instance --name Gmail --internal-urls '^https?://mail.google.com(/.*)?$' 'https://mail.google.com' ~/Software/Gmail
+nativefier --single-instance --name Hangouts --internal-urls '^https?://hangouts.google.com(/.*)?$' 'https://hangouts.google.com/' ~/Software/Hangouts
+nativefier --single-instance --name Messenger 'https://www.messenger.com' ~/Software/Messenger/
+nativefier --single-instance --name OGS 'https://online-go.com' ~/Software/OGS/
+nativefier --single-instance --name Outlook --internal-urls '^https?://mail.dayjob.example.com(/.*)?$' 'https://mail.dayjob.example.com/owa/' ~/Software/Outlook
+nativefier --single-instance --name Skype --internal-urls '^https?://web.skype.com(/.*)?$' 'https://web.skype.com/en/' ~/Software/Skype
+nativefier --name Slack --internal-urls '^https?://(.*\.)?slack.com(/.*)?$' 'https://dayjobapac.slack.com' ~/Software/Slack
+nativefier --single-instance --name Todoist 'https://todoist.com' ~/Software/Todoist/
+nativefier --single-instance --name WhatsApp --internal-urls '^https?://web.whatsapp.com(/.*)?$' 'https://web.whatsapp.com' ~/Software/WhatsApp/
