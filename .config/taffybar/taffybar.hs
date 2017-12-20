1af26fd38b9db88e43e92546bfbe52131b417bb4
Björn Lindström <bkhl@elektrubadur.se>
2017-12-20 17:09:22 +0700

    Add pager to taffybar

--- .config/taffybar/taffybar.hs
+++ .config/taffybar/taffybar.hs
@@ -4,9 +4,14 @@ import           System.Taffybar
 import           System.Taffybar.FreedesktopNotifications
 import           System.Taffybar.SimpleClock
 import           System.Taffybar.Systray
+import           System.Taffybar.TaffyPager
 
 main = do
   let clock = textClockNew Nothing "%a %b %_d %H:%M" 1
       tray = systrayNew
+      pager = taffyPagerNew defaultPagerConfig
   defaultTaffybar
-    defaultTaffybarConfig {startWidgets = [clock], endWidgets = [tray]}
+    defaultTaffybarConfig
+    { startWidgets = [pager]
+    , endWidgets = [tray, clock]
+    }
