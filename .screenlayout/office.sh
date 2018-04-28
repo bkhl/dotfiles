033c651def7a72f8c04dd21fba3db254378bc35d
Björn Lindström <bkhl@elektrubadur.se>
2018-04-28 21:59:56 +0700

    Removed i3 config and some related stuff

--- .screenlayout/office.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-xrandr --addmode DP-2-2 "1680x1050"
-xrandr \
-    --output DP-2-3 --mode 1680x1050 --pos 0x0 --rotate normal \
-    --output DP-2-2 --primary --mode 1680x1050 --pos 1680x0 --rotate normal \
-    --output eDP-1 --mode 1366x768 --pos 3360x0 --rotate normal \
