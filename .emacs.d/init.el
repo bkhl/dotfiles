605d4aa3f3a897a6d53801fd0d319e8bd9f80344
Björn Lindström <bkhl@elektrubadur.se>
2020-10-14 22:55:27 +0700

    Various Emacs configuration

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,7 +1,12 @@
+;; Save custom values in separate file.
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file :noerror)
+
+;; Set up package.el.
 (require 'package)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			 ("gnu" . "http://elpa.gnu.org/packages/")
-			 ("org" . "http://orgmode.org/elpa/")))
+                         ("gnu" . "http://elpa.gnu.org/packages/")
+                         ("org" . "http://orgmode.org/elpa/")))
 (package-initialize)
 
 ;; Ensure that use-package is installed.
