271c72b5ba74afd08e8c5a93728b7d06344d6cbe
Björn Lindström <bkhl@elektrubadur.se>
2020-10-18 10:35:04 +0700

    Remove package-initialize
    Not necessary in Emacs 27.

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -16,7 +16,6 @@
   (setq package-archives '(("melpa" . "https://melpa.org/packages/")
                            ("gnu" . "http://elpa.gnu.org/packages/")
                            ("org" . "http://orgmode.org/elpa/")))
-  (package-initialize)
 
   ;; Ensure that use-package is installed.
   (when (not (package-installed-p 'use-package))
