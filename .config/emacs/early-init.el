e1ae82aadb8effd8f4d9af3020c67c81e119ec84
Björn Lindström <bkhl@elektrubadur.se>
2020-10-18 10:31:22 +0700

    Set Emacs window appearance in early-init.el
    This makes the window look nicer during startup, without the tool and scroll
    bars strobing in and out.

---
+++ .config/emacs/early-init.el
@@ -0,0 +1,5 @@
+;; Disable tool bars and scroll bar. Doing this here in early-init.el means they
+;; won't appear during the startup process.
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+(scroll-bar-mode 0)
