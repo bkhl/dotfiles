e62d967219910359dce47cd90c31f62cd41b454a
Björn Lindström <bkhl@elektrubadur.se>
2020-10-20 20:40:21 +0700

    Added Projectile config and some related stuff to Emacs

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+;; This makes projectile list only tracked files in this repository.
+((nil . ((projectile-git-command . "git ls-files -zc --exclude-standard"))))
