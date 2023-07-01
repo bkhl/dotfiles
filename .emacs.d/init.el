9945553b19dbe298ad2adf84ab613e7520055888
Björn Lindström <bkhl@elektrubadur.se>
2023-07-01 09:54:45 +0200

    Wrap loading of configuration.org in function
    For simpler reloading of the whole file after editing configuration.org

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,12 @@
-;; During loading of init file, disable checking filenames against the list of
-;; filetype handlers. This speeds up startup, as otherwise this list would be
-;; checked for every loaded .el and .elc file.
-(let ((file-name-handler-alist nil))
-  ;; Load the configuration from the Org configuration file.
-  (org-babel-load-file (concat user-emacs-directory "configuration.org")))
+(defun my/load-configuration ()
+  "Load configuration from configuration.org"
+
+  (interactive)
+
+  ;; During loading of init file, disable checking filenames against the list of
+  ;; filetype handlers. This speeds up startup, as otherwise this list would be
+  ;; checked for every loaded .el and .elc file.
+  (let ((file-name-handler-alist nil))
+    (org-babel-load-file (concat user-emacs-directory "configuration.org"))))
+
+(my/load-configuration)
